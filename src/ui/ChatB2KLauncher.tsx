8b681e5cb7c8f25dc2f9423c3f22442919b5412a
EliteFitness101 <resonancefitnessng@gmail.com>
2026-06-23 18:27:06 +0100

    Add ChatB2KLauncher component for assessment link

---
+++ src/ui/ChatB2KLauncher.tsx
@@ -0,0 +1,12 @@
+export default function ChatB2KLauncher() {
+  return (
+    <a
+      href="/assessment"
+      className="fixed bottom-6 right-6 z-50"
+    >
+      <button className="px-6 py-3 rounded-xl bg-yellow-500 font-bold">
+        CHATB2K™ Assessment
+      </button>
+    </a>
+  );
+}
